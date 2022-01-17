305afc117cbd31387e945d15ec2cdf652ac56d13
vv-musatov <vv_musatov@mail.ru>
2022-01-17 22:45:14 +0300

    Lesson 6. Login fix

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 
 from basketapp.models import Basket
@@ -7,6 +8,7 @@ from mainapp.models import Product
 # Create your views here.
 
 
+@login_required
 def basket(request):
     title = "корзина"
     basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
@@ -14,6 +16,7 @@ def basket(request):
     return render(request, "basketapp/basket.html", content)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -27,6 +30,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
