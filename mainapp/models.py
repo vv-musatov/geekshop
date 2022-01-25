90a9751d98d50337329e6fcda7ae80ca1cf35b57
vv-musatov <76052038+vv-musatov@users.noreply.github.com>
2022-01-25 23:31:35 +0300

    Lesson 7 (#9)
    * Lesson_7. Complete.

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name="имя", max_length=64, unique=True)
     description = models.TextField(verbose_name="описание", blank=True)
+    is_active = models.BooleanField(verbose_name="категория активна", default=True)
 
     def __str__(self):
         return self.name
@@ -28,7 +29,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50, verbose_name="номер телефона")
     email = models.EmailField(max_length=254, verbose_name="электронная почта")
     city = models.CharField(max_length=128, default="Москва", verbose_name="город")
-    address = models.CharField(max_length=254, verbose_name="адрес")
+    address = models.CharField(max_length=254, verbose_name="адресс")
 
     def __str__(self):
         return f"{self.pk} {self.email}"
