f2b857be4885f51d7f7fa64891ddc13866f0aeb2
Vladimir <76052038+vv-musatov@users.noreply.github.com>
2022-02-12 18:21:45 +0300

    Lesson 11 (#13)
    Lesson_11. Complete.

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.shortcuts import get_object_or_404
 
 from mainapp.models import Product
 
@@ -30,3 +31,11 @@ class Basket(models.Model):
         _items = Basket.objects.filter(user=self.user)
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
+
+    @staticmethod
+    def get_items(user):
+        return Basket.objects.filter(user=user).order_by("product__category")
+
+    @staticmethod
+    def get_item(pk):
+        return get_object_or_404(Basket, pk=pk)
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "basketapp",
     "adminapp",
     "social_django",
+    "ordersapp",
 ]
 
 # Auth model
@@ -77,6 +78,7 @@ TEMPLATES = [
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
+                "django.template.context_processors.media",
             ],
         },
     },
