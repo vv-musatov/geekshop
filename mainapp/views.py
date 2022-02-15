acd92b21b3eec28268814d0739b14e18b25e9a94
vv-musatov <vv_musatov@mail.ru>
2022-02-15 22:12:21 +0300

    lesson_14. debug-tool-bar

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,19 +12,29 @@ from .models import Contact, Product, ProductCategory
 
 def main(request):
     title = "главная"
-    products = Product.objects.filter(is_active=True, category__is_active=True)[:3]
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
+    products = Product.objects.filter(
+        is_active=True, category__is_active=True)[:3]
+    content = {"title": title, "products": products,
+               "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
-def get_hot_product():
-    products = Product.objects.filter(is_active=True, category__is_active=True)
-    return random.sample(list(products), 1)[0]
+# def get_hot_product():
+#     products = Product.objects.filter(is_active=True, category__is_active=True)
+#     return random.sample(list(products), 1)[0]
 
 
-def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
-    return same_products
+# def get_same_products(hot_product):
+#     same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
+#     return same_products
+
+
+def get_hot_product_list():
+    products = Product.objects.filter(
+        is_active=True, category__is_active=True).select_related("category")
+    hot_product = random.sample(list(products), 1)[0]
+    hot_list = products.exclude(pk=hot_product.pk)[:3]
+    return (hot_product, hot_list)
 
 
 def products(request, pk=None, page=1):
@@ -34,7 +44,8 @@ def products(request, pk=None, page=1):
     if pk is not None:
         if str(pk) == str(0):
             category = {"pk": 0, "name": "все"}
-            products = Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
+            products = Product.objects.filter(
+                is_active=True, category__is_active=True).order_by("price")
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by(
@@ -57,8 +68,9 @@ def products(request, pk=None, page=1):
             "media_url": settings.MEDIA_URL,
         }
         return render(request, "mainapp/products_list.html", content)
-    hot_product = get_hot_product()
-    same_products = get_same_products(hot_product)
+    # hot_product = get_hot_product()
+    # same_products = get_same_products(hot_product)
+    hot_product, same_products = get_hot_product_list()
     content = {
         "title": title,
         "links_menu": links_menu,
@@ -84,5 +96,6 @@ def contact(request):
     title = "о нас"
     visit_date = timezone.now()
     locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    content = {"title": title, "visit_date": visit_date,
+               "locations": locations}
     return render(request, "mainapp/contact.html", content)
