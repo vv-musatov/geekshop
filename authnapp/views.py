a38369d63e7a9306cef3a1f06a4031a1cfcd9b9f
vv-musatov <vv_musatov@mail.ru>
2022-02-17 00:09:14 +0300

    lesson_14. Load testing

--- authnapp/views.py
+++ authnapp/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.db import transaction
 from django.shortcuts import HttpResponseRedirect, render
@@ -57,6 +58,7 @@ def register(request):
     return render(request, "authnapp/register.html", content)
 
 
+@login_required
 @transaction.atomic
 def edit(request):
     title = "редактирование"
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
@@ -16,7 +17,7 @@ from ordersapp.models import Order, OrderItem
 # Create your views here.
 
 
-class OrderList(ListView):
+class OrderList(LoginRequiredMixin, ListView):
     model = Order
 
     def get_queryset(self):
