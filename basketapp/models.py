568cbba2999a7cc95a7e49888241536f0150f2d3
vv-musatov <vv_musatov@mail.ru>
2022-02-27 22:05:54 +0300

    lesson_15. Low level cache

--- basketapp/models.py
+++ basketapp/models.py
@@ -9,13 +9,10 @@ from mainapp.models import Product
 
 
 class Basket(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE, related_name="basket")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="basket")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(
-        verbose_name="количество", default=0)
-    add_datetime = models.DateTimeField(
-        verbose_name="время добавления", auto_now_add=True)
+    quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
+    add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
 
     @property
     def product_cost(self):
