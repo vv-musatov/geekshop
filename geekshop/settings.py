40de05b4a5c6cb4f91b9bd9a933564c13fa50043
Vladimir <76052038+vv-musatov@users.noreply.github.com>
2022-03-02 21:40:30 +0300

    Lesson 15 (#17)
    lesson_15. Complete

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -251,3 +251,18 @@ if DEBUG:
         "template_profiler_panel.panels.template.TemplateProfilerPanel",
     ]
 # <--- Django Debug Toolbar
+
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = "geekbrains"
+
+# Be carefull if you have Windows! Install Memcached before run project!
+#     https://www.ubergizmo.com/how-to/install-memcached-windows/
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
+LOW_CACHE = True
